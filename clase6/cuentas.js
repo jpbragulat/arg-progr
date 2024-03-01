6489a6f580fc3003d68ee675e7972164da91c0cc
Julio Pablo <juliopbragulat@gmail.com>
2024-03-01 17:25:57 -0300

    correciones en cuentas.js
    se crea funcion que pasa nodelist a tipo array asi se pueden llamar las cuentas con cualquier tipo de Array.

--- clase6/cuentas.js
+++ clase6/cuentas.js
@@ -1,31 +1,32 @@
-function calcularNumeroMayor($integrantes){
-    let numeroMayor = $integrantes[0].value;
-    for (let i=1; i<$integrantes.length; i++){
-        if ($integrantes[i].value > numeroMayor){
-            numeroMayor = $integrantes[i].value;
+function calcularNumeroMayor(numeros){
+    let numeroMayor = numeros[0];
+    for (let i=1; i<numeros.length; i++){
+        if (numeros[i] > numeroMayor){
+            numeroMayor = numeros[i];
         }
     }
+    console.log(numeros[0].value);
     return numeroMayor;
 } 
 
-function calcularNumeroMenor($integrantes){
-    let numeroMenor = $integrantes[0].value;
-    for (let i=0; i<$integrantes.length; i++){
-        if($integrantes[i].value < numeroMenor){
-            numeroMenor = $integrantes[i].value;
+function calcularNumeroMenor(numeros){
+    let numeroMenor = numeros[0];
+    for (let i=0; i<numeros.length; i++){
+        if(numeros[i] < numeroMenor){
+            numeroMenor = numeros[i];
         }
     }
     return numeroMenor;
 }
 
-function calcularPromedio($integrantes){
+function calcularPromedio(numeros){
     let totalSuma=0;
     let promedio=0;
-    for(let i=0; i<$integrantes.length; i++){
-        totalSuma = Number(totalSuma) + Number($integrantes[i].value);
+    for(let i=0; i<numeros.length; i++){
+        totalSuma = Number(totalSuma) + Number(numeros[i]);
         console.log("total suma en bucle for: ", totalSuma);
     }
-    promedio = totalSuma / $integrantes.length;
+    promedio = totalSuma / numeros.length;
     return promedio;
    
     
