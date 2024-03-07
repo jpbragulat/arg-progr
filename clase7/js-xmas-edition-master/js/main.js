3780adfff2903b0cee629eb67b32f4711ff4f94d
Julio Pablo <juliopbragulat@gmail.com>
2024-03-07 00:14:05 -0300

    s

--- clase7/js-xmas-edition-master/js/main.js
+++ clase7/js-xmas-edition-master/js/main.js
@@ -3,10 +3,20 @@ const nombre = $form.nombre.value;
 const ciudades = $form.ciudad.value;
 
 function validarNombre(){
-    if(nombre.length === 0){
-        console.log("El nombre debe tener ma");
+    if(nombre.length < 2){
+        console.log("El nombre debe contener al menos 2 caracteres.");
+        return 'El nombre debe contener al menos 2 caracteres.';
     }
+    else if(nombre.length > 150 ){
+        console.log("El nombre no puede contener mas de 150 caracteres.");
+        return 'El nombre no puede contener mas de 150 caracteres.';
+    }
+    else {
+        return '';
+    }
+    
 }
 
+$form.onsubmit = validarNombre();
 console.log(nombre);
 console.log(ciudades);
